76b8c0a649436fd2d980a2f9c4358321df8d6b62
poopburger78 <poopburger78@gmail.com>
2025-09-18 22:27:11 -0700

    Initial Commit

---
+++ vocab extensionTests/vocab_extensionTests.swift
@@ -0,0 +1,17 @@
+//
+//  vocab_extensionTests.swift
+//  vocab extensionTests
+//
+//  Created by Maddy Ludes on 9/18/25.
+//
+
+import Testing
+@testable import vocab_extension
+
+struct vocab_extensionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
